216c997a13231b1a0c3c97da2c62880131b3ab52
fetaoily <fetaoily@gmail.com>
2018-11-07 18:03:24 +0800

    1. add YxExtDashLine.js

--- examples/index.vue
+++ examples/index.vue
@@ -23,17 +23,26 @@
       initFabricCanvas () {
         let width = window.innerWidth * 0.9;
         let height = window.innerHeight * 0.9;
-        this.canvas = new fabric.Canvas('c', {
+        window.__canvas = this.canvas = new fabric.Canvas('c', {
             width, height
           }
         );
         let rect = new fabric.Rect({
           top: 100,
           left: 100,
-          width: 60, height: 70,
-          fill: 'red'
+          fill: 'red',
+          width: 60,
+          height: 70
+        });
+        let dashLine = new fabric.YxExtDashLine([10, 10, 300, 300], {
+          fill: 'red',
+          stroke: 'red',
+          strokeWidth: 5,
+          selectable: true
         });
         this.canvas.add(rect);
+        this.canvas.add(dashLine);
+        this.canvas.renderAll();
       }
     }
   }
