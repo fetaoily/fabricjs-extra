0bf47e02a0fd7b0bf6626fd4a2e53900290430ff
fetaoily <fetaoily@gmail.com>
2018-11-08 12:50:17 +0800

    1. add : YxExtEraserPath.js, YxExtEraserBrush.js 2. refactor : YxExtDashLine.js, index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const YxExtDashLine = require('./lib/YxExtDashLine');
-
-module.exports = { YxExtDashLine };
+/* eslint-disable */
+const { fabric } = require('fabric');
+require('./lib/YxExtDashLine')({ fabric });
+require('./lib/YxExtEraserPath')({ fabric });
+require('./lib/YxExtEraserBrush')({ fabric });
+module.exports = { fabric };
