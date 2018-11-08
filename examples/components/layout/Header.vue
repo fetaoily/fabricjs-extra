21ea99ae5be800f75bb038fbefeaa0e32c0bfe3f
fetaoily <fetaoily@gmail.com>
2018-11-08 17:33:28 +0800

    1. clearCanvas

--- examples/components/layout/Header.vue
+++ examples/components/layout/Header.vue
@@ -3,6 +3,9 @@
     <el-button-group>
       <el-button v-on:click="setDrawingModeForBrush">画笔</el-button>
       <el-button v-on:click="setDrawingModeForEraser">橡皮</el-button>
+      <el-button v-on:click="">撤销</el-button>
+      <el-button v-on:click="">回撤</el-button>
+      <el-button v-on:click="clearCanvas">清屏</el-button>
     </el-button-group>
   </header>
 </template>
@@ -15,8 +18,10 @@
         window.__canvas.setDrawingModeForBrush()
       },
       setDrawingModeForEraser () {
-        // window.__canvas.setDrawingModeForEraser({ width: 10 })
-        window.__canvas.setDrawingModeForEraser({})
+        window.__canvas.setDrawingModeForEraser({ width: 30 })
+      },
+      clearCanvas(){
+        window.__canvas.clear();
       }
     }
   }
